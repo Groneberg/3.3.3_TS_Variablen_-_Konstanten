495a552caa872f819795121be136739c738504e3
Stefan Groneberg <stefangroneberg@Mac.fritz.box>
2025-06-03 12:52:45 +0200

    chance value of spa

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d  = 20;
+  String timeStr = "Zeit";
+  int seven  = 7;
+  int twelve  = 12;
+  String b = "!";
+  int a  = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+ 
+}
